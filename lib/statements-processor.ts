df9006622be0fe8d3620e3757c28e640baa7a13c
JohannIsaac <johann.isaac.go@gmail.com>
2024-08-30 03:11:01 +0700

    Gitignore and minor fix

--- lib/statements-processor.ts
+++ lib/statements-processor.ts
@@ -64,7 +64,7 @@ export default class StatementsProcessor {
 
 
 	// Recursive function for getting nested partials with pathname
-	private processStatements(templateData: TemplateData, partialsMap: SourceDataMap = new Map(), helpersMap: PathMap = new Map) {
+	private processStatements(templateData: TemplateData, partialsMap: SourceDataMap = new Map(), helpersMap: PathMap = new Map()) {
 		
 		// If partials are detected, process each partial
 		const { partials, helpers } = this.getAllStatements(templateData.source)
